1135024e7a5de36b52243241d8b761892c4d9717
Tram Anh <tramanh0105@gmail.com>
2019-04-02 16:51:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var nodeList = document.querySelectorAll(".drum");
+var count =0;
+
+//Adressing different drums to different pressed keys
+function keyPress( key ){
+  switch (key) {
+    case "w":
+      var crash = new Audio ('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio ('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio ('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio ('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio ('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio ('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio ('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    default:
+      alert("Unvalid input!");
+
+  }
+}
+
+//zuweisen die EventListener auf den Buttons
+while(count <nodeList.length){
+  document.querySelectorAll(".drum")[count].addEventListener("click", function(){
+    var clickedButton = this.innerHTML;
+    keyPress(clickedButton);
+  });
+  count ++;
+}
+document.addEventListener("keypress",function(event){
+  var char = event.key;
+  keyPress(char);
+})
